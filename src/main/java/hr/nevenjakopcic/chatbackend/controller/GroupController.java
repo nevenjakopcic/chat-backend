d84d25647c3e581880e7b98d2e423e66c8a7deff
Neven Jakopčić <nevenjakopcic@gmail.com>
2023-08-12 18:30:30 +0200

    Fix CORS

--- src/main/java/hr/nevenjakopcic/chatbackend/controller/GroupController.java
+++ src/main/java/hr/nevenjakopcic/chatbackend/controller/GroupController.java
@@ -5,6 +5,7 @@ import hr.nevenjakopcic.chatbackend.service.GroupService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/group")
 @AllArgsConstructor
+@CrossOrigin
 public class GroupController {
 
     private final GroupService groupService;
--- src/main/java/hr/nevenjakopcic/chatbackend/controller/UserController.java
+++ src/main/java/hr/nevenjakopcic/chatbackend/controller/UserController.java
@@ -5,14 +5,12 @@ import hr.nevenjakopcic.chatbackend.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
 @AllArgsConstructor
+@CrossOrigin
 public class UserController {
 
     private final UserService userService;
